91f7828c43609a673b7fc52e0c8646b4015bebd6
askfiy <c2323182108@gmail.com>
2022-03-18 00:40:23 +0800

    change surround configature

--- lua/configure/plugins/surround.lua
+++ lua/configure/plugins/surround.lua
@@ -1,7 +1,8 @@
 -- https://github.com/ur4ltz/surround.nvim
 
 local options = {
-    mappings_style = "surround"
+    mappings_style = "surround",
+    space_on_closing_char = true
 }
 
 require("surround").setup(options)
