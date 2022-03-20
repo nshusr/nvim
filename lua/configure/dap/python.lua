c374e36d6d0e1ba3808288f5fd53302707040da6
askfiy <c2323182108@gmail.com>
2022-03-20 22:56:53 +0800

    change dap python configature

--- lua/configure/dap/python.lua
+++ lua/configure/dap/python.lua
@@ -12,9 +12,19 @@ return {
             request = "launch",
             name = "Launch file",
             program = "${file}",
-            pythonPath = function()
-                return vim.u.python_interpreter_path
-            end
+            pythonPath = "python3"
+        },
+        {
+            type = "python",
+            request = "launch",
+            name = "Launch Django",
+            program = vim.fn.getcwd() .. "/manage.py",
+            pythonPath = "python3",
+            args = {
+                "runserver",
+                "127.0.0.1:8001"
+            },
+            django = true
         }
     }
 }
